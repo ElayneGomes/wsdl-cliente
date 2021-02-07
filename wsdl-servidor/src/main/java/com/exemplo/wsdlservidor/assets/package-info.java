a8348290c89e28752834a44e6a806d07926a9476
ElayneGomes <elaynegomes@gmail.com>
2021-02-06 21:04:57 -0300

    Delete wsdl-servidor directory

--- wsdl-servidor/src/main/java/com/exemplo/wsdlservidor/assets/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
-// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.01.30 às 03:06:33 AM BRT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://exemplo.com/wsdlservidor/assets", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.exemplo.wsdlservidor.assets;
